564042eb01429d84d9a39fae994109e4245168d0
Charles Tay <charles.tay@lumiq.com>
2022-11-14 21:08:51 +0800

    feat: change to node-fetch

--- pages/api/cat/breeds/index.ts
+++ pages/api/cat/breeds/index.ts
@@ -1,13 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { Breed } from '../../../../types/types';
-import { getBreeds } from '../../../../services/thecatapi';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Breed } from "../../../../types/types";
+import { getBreeds } from "../../../../services/thecatapi";
 
 export default async function handler(
   _: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   const response = await getBreeds();
-  const breedData: Breed[] = await response.json();
+  const breedData = (await response.json()) as Breed[];
 
   // TODO: consider manipulation in the BE to reduce data being sent over the wire
 
--- services/thecatapi.ts
+++ services/thecatapi.ts
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 const headers = { "x-api-key": process.env.THE_CAT_API_KEY || "" };
 
 const ROOT_URL = "https://api.thecatapi.com/v1";
@@ -12,14 +13,10 @@ export const getBreeds = () =>
     headers,
   });
 
-export const getCatsByBreedId = (breedId: string, numberToGet: number = 6) => {
-  console.log(
-    `${ROOT_URL}/images/search?breedIds=${breedId}&limit=${numberToGet.toString()}`
-  );
+export const getCatsByBreedId = (breedId: string, numberToGet: number = 6) =>
   fetch(
     `${ROOT_URL}/images/search?breedIds=${breedId}&limit=${numberToGet.toString()}`,
     {
       headers,
     }
   );
-};
