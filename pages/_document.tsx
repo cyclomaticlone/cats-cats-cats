00d9f399a639338db4209ce4f487c8991f0f6b16
Charles Tay <charles.tay@lumiq.com>
2022-11-15 08:12:45 +0800

    feat: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <body className="bg-stone-900">
         <Main />
         <NextScript />
